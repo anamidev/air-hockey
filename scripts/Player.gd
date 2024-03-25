b1d553f5a77d6d5f7c4fc03a484516f54b03e191
anamidev <anastasiia.n.minaeva@gmail.com>
2024-03-25 05:21:36 +0500

    Player now uses 2D physics and collisions

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -1,13 +1,12 @@
 extends CharacterBody2D
 
 
+const VELOCITY_MOD := 15.0
+
+
 var is_hovered = false
 var is_dragged = false
-
-
-func _process(delta):
-	if is_dragged:
-		global_position = get_global_mouse_position()
+var last_mouse_position = Vector2(0,0)
 
 
 func _on_mouse_entered():
@@ -21,6 +20,15 @@ func _on_mouse_exited():
 func _on_input_event(viewport, event, shape_idx):
 	if is_hovered and Input.is_action_pressed("mouse_left_click"):
 		is_dragged = true
-	else:
-		is_dragged = false
 
+
+func _process(delta):
+	if is_dragged:
+		last_mouse_position = get_local_mouse_position()
+		
+	if not Input.is_action_pressed("mouse_left_click"):
+		is_dragged = false
+		
+	velocity = last_mouse_position * VELOCITY_MOD
+	move_and_slide()
+	
